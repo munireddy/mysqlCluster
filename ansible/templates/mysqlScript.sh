793a6d1392b882c9a3818732144076f7537eac2e
Ubuntu <ubuntu@ip-172-31-27-254.us-east-2.compute.internal>
2018-07-16 20:18:29 +0000

    Updated file for Three node cluster

--- ansible/templates/mysqlScript.sh
+++ ansible/templates/mysqlScript.sh
@@ -3,13 +3,6 @@ mysql -u root  -e STATUS
 mysql <<EOFMYSQL
 use mysql;
 select user, host from mysql.user;
-SET GLOBAL server_id=51;
-SET GLOBAL connect_timeout=120;
-SET GLOBAL binlog_checksum=NONE;
-SET GLOBAL enforce_gtid_consistency=ON;
-SET GLOBAL gtid_mode=OFF_PERMISSIVE;
-SET GLOBAL gtid_mode=ON_PERMISSIVE;
-SET GLOBAL gtid_mode=ON;
 
 alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'passw0rd';
 
